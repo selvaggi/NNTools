1c01c1dbcd2978add03bb66a36de30bb74fad4bf
Huilin Qu <huilin.qu@cern.ch>
2018-07-10 15:01:57 +0200

    Update preprocessing configs and remove outdated stuff.

--- preprocessing/data_formats/ak8_list.py
+++ preprocessing/data_formats/ak8_list.py
@@ -2,17 +2,15 @@
 Description on how to produce metadata file.
 '''
 
-input_filter = None
+input_filter = r'Tau|htata'
 treename = 'deepntuplizer/tree'
 reweight_events = -1
 reweight_bins = [list(range(200, 2051, 50)), [-10000, 10000]]
 metadata_events = 1000000
-selection = '''jet_tightId \
-&& ( !label_H_cc )'''
-# && ( (sample_isQCD && fj_isQCD) || (!sample_isQCD && !fj_isQCD)) \
+selection = '''jet_tightId'''
 var_groups = {
     # 'group_name': ( ('regex1', 'regex2', ...), list_length )
-    'part': (('part_',), 150),
+    'part': (('part_',), 100),
     'sv': (('sv_',), 7),
     }
 var_blacklist = [
@@ -23,20 +21,23 @@ var_blacklist = [
     'fj_isNonBB',
 
     'n_parts',
-    'n_pfcands',
     'n_tracks',
     'n_sv',
     ]
 var_no_transform_branches = [
+    'event_no',
     'fj_labelJMAR', 'fjJMAR_gen_pt', 'fjJMAR_gen_eta', 'fjJMAR_gen_pdgid',
     'fj_label',
     'fj_isQCD', 'fj_isTop', 'fj_isW', 'fj_isZ', 'fj_isH',
     'npv',
-    'n_parts', 'n_pfcands', 'n_tracks', 'n_sv',
+    'n_parts', 'n_sv',
     'fj_pt', 'fj_eta', 'fj_phi', 'fj_mass',
     'fj_n_sdsubjets', 'fj_nbHadrons', 'fj_ncHadrons',
-    'fjPuppi_tau21', 'fjPuppi_tau32', 'fjPuppi_corrsdmass',
+    'fjPuppi_tau21', 'fjPuppi_tau32', 'fjPuppi_corrsdmass', 'fjPuppi_sdmass',
     'fj_doubleb', 'pfCombinedInclusiveSecondaryVertexV2BJetTags',
+    'fj_genjet_pt', 'fj_genOverReco_pt', 'fj_genOverReco_pt_null',
+    'fj_genjet_mass', 'fj_genOverReco_mass', 'fj_genOverReco_mass_null',
+    'fj_genjet_sdmass', 'fj_genjet_sdmass_sqrt', 'fj_genOverReco_sdmass', 'fj_genOverReco_sdmass_null',
 
     "fj_tau21",
     "fj_tau32",
@@ -95,11 +96,17 @@ var_no_transform_branches = [
 label_list = ['label_Top_bcq', 'label_Top_bqq', 'label_Top_bc', 'label_Top_bq',
               'label_W_cq', 'label_W_qq',
               'label_Z_bb', 'label_Z_cc', 'label_Z_qq',
-              'label_H_bb', 'label_H_qqqq',
+              'label_H_bb', 'label_H_cc', 'label_H_qqqq',
               'label_QCD_bb', 'label_QCD_cc', 'label_QCD_b', 'label_QCD_c', 'label_QCD_others',
               ]
 reweight_var = ['fj_pt', 'fjPuppi_corrsdmass']
-reweight_classes = ['fj_isTop', 'fj_isW', 'fj_isZ', 'fj_isH', 'fj_isQCD']
+reweight_classes = [
+    'fj_isTop',
+    'fj_isW',
+    'fj_isZ',
+    'label_H_bb', 'label_H_cc', 'label_H_qqqq',  # H_bb, H_cc class_wgt divided by 2, H_qqqq devided by ~4-5 to become 1
+    'fj_isQCD',
+    ]
 reweight_method = 'flat'
 var_img = None
 var_pos = None
